f39904bf69240641850a1351992736f721aaa562
{soommmin} <tnakszla@gmail.com>
2024-02-20 20:06:09 +0900

    Main UI 그리기

--- app/src/main/java/android/part3_chapter1/MainActivity.kt
+++ app/src/main/java/android/part3_chapter1/MainActivity.kt
@@ -2,10 +2,21 @@ package android.part3_chapter1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.part3_chapter1.databinding.ActivityMainBinding
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startDetectButton.setOnClickListener {
+            it.isVisible = false
+        }
     }
 }
